336eef2ddc5e4838c3ba576453cab7d9bee15068
OCTO <chobotnicecz@gmail.com>
2025-03-10 17:21:04 +0100

    refactor

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -5,32 +5,13 @@ import RenderPage from "./components/renderPage/renderPage";
 import Button from "./components/Button/Button";
 
 function App() {
-  const [switcher, setSwitcher] = useState(false);
-  const [customColor, setNewColor] = useState("customBTN");
-  const [message, setNewMessage] = useState("Click");
   const [currentPage, setCurrentPage] = useState(1);
-
   const maxPagesCount = 2;
-
-  const switcherBtn = () => {
-    setSwitcher(!switcher);
-    return !switcher;
-  };
-
-  const changeMessage = () => {
-    if (switcherBtn()) {
-      setNewMessage("Ahoj!");
-      setNewColor("placeholder");
-    } else {
-      setNewMessage("Click");
-      setNewColor("customBTN");
-    }
-  };
 
   const changePage = () => {
     if (currentPage === maxPagesCount) {
       setCurrentPage(1);
-    } else if (currentPage < maxPagesCount) {
+    } else {
       setCurrentPage(currentPage + 1);
     }
   };
@@ -38,14 +19,8 @@ function App() {
   return (
     <div>
       <div className="wrapper">
-        <RenderPage
-          customColor={customColor}
-          message={message}
-          changeMessage={changeMessage}
-          currentPage={currentPage}
-        />
-
-        <Button onClick={changePage} />
+        <RenderPage currentPage={currentPage} />
+        <Button onClick={changePage}>next</Button>
       </div>
     </div>
   );
--- app/src/components/Pages/Page01_Click.jsx
+++ app/src/components/Pages/Page01_Click.jsx
@@ -1,6 +1,26 @@
+import { useState } from "react";
 import Button from "../Button/Button";
 
-const Page01_Click = ({ customColor, message, changeMessage }) => {
+const Page01_Click = () => {
+  const [switcher, setSwitcher] = useState(false);
+  const [customColor, setNewColor] = useState("customBTN");
+  const [message, setNewMessage] = useState("Click");
+
+  const switcherBtn = () => {
+    setSwitcher(!switcher);
+    return !switcher;
+  };
+
+  const changeMessage = () => {
+    if (switcherBtn()) {
+      setNewMessage("Ahoj!");
+      setNewColor("placeholder");
+    } else {
+      setNewMessage("Click");
+      setNewColor("customBTN");
+    }
+  };
+
   return (
     <Button customColor={customColor} onClick={changeMessage}>
       {message}
